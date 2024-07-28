236a71256ef2eb247d1d241bbb826b15bc6f6e81
zainebhn <heni.zeineb@gmail.com>
2024-07-28 15:32:27 +0100

    Initial commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { SUBRESOURCE_INTEGRITY_MANIFEST } from 'next/dist/shared/lib/constants';
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
